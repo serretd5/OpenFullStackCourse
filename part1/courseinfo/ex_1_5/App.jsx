dffd316cb994780a562cd17d77bd7532cb0ea0ce
serretd5 <ericserretvalde@gmail.com>
2024-07-14 12:11:56 +0200

    new file:   part1/courseinfo/ex_1_1/App.css 	new file:   part1/courseinfo/ex_1_1/App.jsx 	new file:   part1/courseinfo/ex_1_1/ex1_1 	new file:   part1/courseinfo/ex_1_1/index.css 	new file:   part1/courseinfo/ex_1_1/main.jsx 	new file:   part1/courseinfo/ex_1_2/App.css 	new file:   part1/courseinfo/ex_1_2/App.jsx 	new file:   part1/courseinfo/ex_1_2/index.css 	new file:   part1/courseinfo/ex_1_2/main.jsx#     new file:   part1/courseinfo/ex_1_3/App.css 	new file:   part1/courseinfo/ex_1_3/App.jsx 	new file:   part1/courseinfo/ex_1_3/index.css 	new file:   part1/courseinfo/ex_1_3/main.jsx 	new file:   part1/courseinfo/ex_1_4/App.css 	new file:   part1/courseinfo/ex_1_4/App.jsx 	new file:   part1/courseinfo/ex_1_4/index.css 	new file:   part1/courseinfo/ex_1_4/main.jsx 	new file:   part1/courseinfo/ex_1_5/App.css 	new file:   part1/courseinfo/ex_1_5/App.jsx 	new file:   part1/courseinfo/ex_1_5/index.css 	new file:   part1/courseinfo/ex_1_5/main.jsx

---
+++ part1/courseinfo/ex_1_5/App.jsx
@@ -0,0 +1,65 @@
+
+
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <p>
+        {props.part1} {props.exercises1}
+      </p>
+      <p>
+        {props.part2} {props.exercises2}
+      </p>
+      <p>
+        {props.part3} {props.exercises3}
+      </p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>
+       Number of exercises: {props.suma}
+      </p>
+    </div>
+  )
+}
+
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+  return (
+    <div>
+      <Header course={course['name']}/>
+      <Content part1={course['parts'][0]['name']} exercises1 = {course['parts'][0]['exercises']} part2={course['parts'][1]['name']} exercises2 = {course['parts'][1]['exercises']} part3={course['parts'][2]['name']} exercises3 = {course['parts'][2]['exercises']} />
+      <Total suma = {course['parts'][0]['exercises'] + course['parts'][1]['exercises'] + course['parts'][2]['exercises']}/>
+    </div>
+  )
+}
+
+export default App
