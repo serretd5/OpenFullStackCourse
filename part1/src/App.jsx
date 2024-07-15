2ddf3e06ee14367e23f2e1345b2632fb36fa66a6
serretd5 <ericserretvalde@gmail.com>
2024-07-15 04:39:36 +0200

    modified:   part1/src/App.jsx

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,65 +1,32 @@
 
 
-const Header = (props) => {
+const Hello = (props) => {
+
+  const name = props.name
+  const age = props.age
+
+
+  const bornYear = () => new Date().getFullYear() - age
+
   return (
     <div>
-      <h1>{props.course}</h1>
+
+      <p>Hello {name}, you are {age} years old</p>
+      <p>So you were probably born in {bornYear()}</p>
     </div>
   )
 }
-
-const Content = (props) => {
-  return (
-    <div>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
-    </div>
-  )
-}
-
-const Total = (props) => {
-  return (
-    <div>
-      <p>
-       Number of exercises: {props.suma}
-      </p>
-    </div>
-  )
-}
-
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  const name = 'Peter'
+  const age = 10
+
   return (
     <div>
-      <Header course={course['name']}/>
-      <Content part1={course['parts'][0]['name']} exercises1 = {course['parts'][0]['exercises']} part2={course['parts'][1]['name']} exercises2 = {course['parts'][1]['exercises']} part3={course['parts'][2]['name']} exercises3 = {course['parts'][2]['exercises']} />
-      <Total suma = {course['parts'][0]['exercises'] + course['parts'][1]['exercises'] + course['parts'][2]['exercises']}/>
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
     </div>
   )
 }
-
 export default App
