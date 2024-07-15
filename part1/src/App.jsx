32fa3799ee9ea9684fb47cd42dc4481cd748bdf1
serretd5 <ericserretvalde@gmail.com>
2024-07-15 04:49:38 +0200

    modified:   part1/src/App.jsx

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,32 +1,20 @@
 
 
-const Hello = (props) => {
-
-  const name = props.name
-  const age = props.age
-
-
-  const bornYear = () => new Date().getFullYear() - age
-
-  return (
-    <div>
-
-      <p>Hello {name}, you are {age} years old</p>
-      <p>So you were probably born in {bornYear()}</p>
-    </div>
-  )
-}
+import { useState } from 'react'
 
 const App = () => {
-  const name = 'Peter'
-  const age = 10
+
+  const [ counter, setCounter ] = useState(0)
+
+
+  setTimeout(
+    () => setCounter(counter + 1),
+    1000
+  )
 
   return (
-    <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={name} age={age} />
-    </div>
+    <div>{counter}</div>
   )
 }
+
 export default App
