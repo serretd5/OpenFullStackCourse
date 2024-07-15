6a4bcd71cb6e4d663308a4fd104f91d6661f63ac
serretd5 <ericserretvalde@gmail.com>
2024-07-15 05:38:51 +0200

    modified:   part1/src/App.jsx

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,29 +1,44 @@
 
 
 import { useState } from 'react'
+const Display = ({ counter }) => <div>{counter}</div>
+
+const Button = (props) => {
+  return (
+    <button onClick={props.onClick}>
+      {props.text}
+    </button>
+  )
+}
+
+
 
 const App = () => {
   const [ counter, setCounter ] = useState(0)
 
-
   const increaseByOne = () => setCounter(counter + 1)
-  
+
+  const decreaseByOne = () => setCounter(counter - 1)
   const setToZero = () => setCounter(0)
-  console.log('rendering...', counter)
 
   return (
     <div>
-      <div>{counter}</div>
+      <Display counter={counter}/>
 
-      <button onClick={increaseByOne}>
-        plus
-      </button>
-
-      <button onClick={setToZero}>
-        zero
-      </button>
+      <Button
+        onClick={increaseByOne}
+        text='plus'
+      />
+      <Button
+        onClick={setToZero}
+        text='zero'
+      />     
+      <Button
+        onClick={decreaseByOne}
+        text='minus'
+      />           
     </div>
   )
 }
-  console.log('rendering...', counter)
+
 export default App
