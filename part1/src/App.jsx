bcb574ee64b92992fc8dde9a91a6756bcc34c6b4
serretd5 <ericserretvalde@gmail.com>
2024-07-17 05:11:45 +0200

    modified:   part1/src/App.jsx

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -4,19 +4,22 @@ import { useState } from 'react'
 const App = () => {
   const [left, setLeft] = useState(0)
   const [right, setRight] = useState(0)
-
   const [allClicks, setAll] = useState([])
 
+  const [total, setTotal] = useState(0)
 
   const handleLeftClick = () => {
     setAll(allClicks.concat('L'))
     setLeft(left + 1)
-  }
 
+    setTotal(left + right)
+  }
 
   const handleRightClick = () => {
     setAll(allClicks.concat('R'))
     setRight(right + 1)
+
+    setTotal(left + right)
   }
 
   return (
@@ -25,8 +28,9 @@ const App = () => {
       <button onClick={handleLeftClick}>left</button>
       <button onClick={handleRightClick}>right</button>
       {right}
-
       <p>{allClicks.join(' ')}</p>
+
+      <p>total {total}</p>
     </div>
   )
 }
