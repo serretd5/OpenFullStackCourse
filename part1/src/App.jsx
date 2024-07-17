df168511764dc07574482d9229a41732a6e3db0e
serretd5 <ericserretvalde@gmail.com>
2024-07-17 05:16:35 +0200

    modified:   part1/src/App.jsx

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -5,20 +5,17 @@ const App = () => {
   const [left, setLeft] = useState(0)
   const [right, setRight] = useState(0)
   const [allClicks, setAll] = useState([])
-
   const [total, setTotal] = useState(0)
 
   const handleLeftClick = () => {
     setAll(allClicks.concat('L'))
     setLeft(left + 1)
-
     setTotal(left + right)
   }
 
   const handleRightClick = () => {
     setAll(allClicks.concat('R'))
     setRight(right + 1)
-
     setTotal(left + right)
   }
 
@@ -29,7 +26,6 @@ const App = () => {
       <button onClick={handleRightClick}>right</button>
       {right}
       <p>{allClicks.join(' ')}</p>
-
       <p>total {total}</p>
     </div>
   )
