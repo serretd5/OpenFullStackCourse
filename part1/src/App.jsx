73fe3e83274ffdb3b75a02d6ab938ddcd26548f9
serretd5 <ericserretvalde@gmail.com>
2024-07-17 05:09:25 +0200

    modified:   part1/src/App.jsx

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -5,16 +5,28 @@ const App = () => {
   const [left, setLeft] = useState(0)
   const [right, setRight] = useState(0)
 
+  const [allClicks, setAll] = useState([])
+
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
+
   return (
     <div>
       {left}
-      <button onClick={() => setLeft(left + 1)}>
-        left
-      </button>
-      <button onClick={() => setRight(right + 1)}>
-        right
-      </button>
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
       {right}
+
+      <p>{allClicks.join(' ')}</p>
     </div>
   )
 }
