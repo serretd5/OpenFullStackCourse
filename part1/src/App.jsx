a42b22b40e2cabe31771876173c8202a7dec7894
serretd5 <ericserretvalde@gmail.com>
2024-07-20 11:10:30 +0200

    modified:   part1/src/App.jsx

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -9,23 +9,34 @@ const App = () => {
   const [bad, setBad] = useState(0)
   const [total, setTotal] = useState(0)
   const [percent, setPercent] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [sentiment, setSentiment] = useState(0)
 
   const handleGoodClick = () => {
     setGood(good + 1)
     setTotal(total + 1)
-    setPercent(good/total)
+    setPercent((good + 1)/(total + 1))
+    setSentiment(sentiment+1)
+    console.log('value now', good)
+    console.log('value now', bad)
+    console.log('value now', neutral)
+    console.log('value now', total)
+    setAverage((sentiment+1)/(total+1))
   }
 
   const handleNeutralClick = () => {
     setNeutral(neutral + 1)
-    setTotal(total+1)
-    setPercent(good/total)
+    setTotal(total + 1)
+    setPercent((good)/(total + 1))
+    setAverage((sentiment)/(total + 1))
   }
 
   const handleBadClick = () => {
     setBad(bad + 1)
     setTotal(total + 1)
-    setPercent(good/total)
+    setPercent(good/(total + 1))
+    setSentiment(sentiment - 1)
+    setAverage((sentiment - 1)/(total + 1))
   }
 
   return (
@@ -38,6 +49,7 @@ const App = () => {
       <p>Neutral: {neutral}</p>
       <p>Bad: {bad}</p>
       <p>All: {total}</p>
+      <p>Average: {average}</p>
       <p>Percent: {percent}</p>
       
 
