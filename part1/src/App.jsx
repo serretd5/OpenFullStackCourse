0be23a649db61334459b6ed9ce21e84c70fca25a
serretd5 <ericserretvalde@gmail.com>
2024-07-20 10:36:09 +0200

    modified:   part1/src/App.jsx

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -8,9 +8,26 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const handleGoodClick = () => {
+    setGood(good + 1)
+  }
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const handleBadClick = () => {
+    setBad(bad + 1)
+  }
+
   return (
     <div>
-      code here
+      {good}
+      <button onClick={handleGoodClick}>good</button>
+      {neutral}
+      <button onClick={handleNeutralClick}>neutral</button>
+      {bad}
+      <button onClick={handleBadClick}>bad</button>
     </div>
   )
 }
